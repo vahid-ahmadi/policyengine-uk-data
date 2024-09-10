a15d5965e8718c70ac448aad39bb1cdf0390ff1b
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2024-09-10 12:53:08 +0100

    Add initial docs

--- docs/Home.py
+++ docs/Home.py
@@ -3,11 +3,10 @@ import streamlit as st
 st.title("PolicyEngine-UK-Data")
 
 st.write(
-    """PolicyEngine-UK-Data is a package to create representative microdata for the UK, designed for input in the PolicyEngine tax-benefit microsimulation model."""
-)
-
-st.subheader("What does this repo do?")
-
-st.write(
-    """This package creates a (partly synthetic) dataset of households (with incomes, demographics and more) that describes the U.K. household sector. This dataset synthesises multiple sources of data (the Current Population Survey, the IRS Public Use File, and administrative statistics) to improve upon the accuracy of **any** of them."""
+    """
+    PolicyEngine-UK-Data is a package that creates representative microdata for the UK, 
+    designed for input in the PolicyEngine tax-benefit microsimulation model. This tool 
+    allows users to explore the data sources, validation processes, and enhancements 
+    made to ensure accurate and reliable microsimulation results.
+    """
 )
